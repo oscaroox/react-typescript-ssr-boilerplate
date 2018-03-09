252619fd26546ba50d36006491f04d5c9b7b300b
oscar <druiventakoscar@gmail.com>
2018-03-09 20:57:58 +0100

    add page components

---
+++ src/components/AboutPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class AboutPage extends React.Component<any, any> {
+  public render() {
+    return (
+      <div>
+        About Page
+      </div>
+    );
+  }
+}
---
+++ src/components/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class HomePage extends React.Component<any, any> {
+  public render() {
+    return (
+      <div>
+        Home Page
+      </div>
+    );
+  }
+}
