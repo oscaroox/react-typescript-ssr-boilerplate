80c75d3f191cc0e464d400b878a0df884db2157f
oscar <druiventakoscar@gmail.com>
2018-03-10 03:26:10 +0100

    include css in tag when starting in production

--- server/render.tsx
+++ server/render.tsx
@@ -26,6 +26,7 @@ export default function serverRenderer(stats: any): express.RequestHandler {
         res.send(`
             <html>
                 <head>
+                ${assets.getStyles()}
                 </head>
                 <body>
                     <div id="root"></div>
@@ -39,6 +40,7 @@ export default function serverRenderer(stats: any): express.RequestHandler {
 function assetBuilder(assetStats: any, modules: any[]) {
 
     const buildTag = (link: string) => `<script src="/static/${link}"></script>`;
+    const buildStyle = (link: string) => `<link rel="stylesheet" type="text/css" href="/static/${link}">`;
 
     const chunks = assetStats && assetStats.assetsByChunkName;
 
@@ -49,7 +51,7 @@ function assetBuilder(assetStats: any, modules: any[]) {
             }
 
             // vendor shoudl be the first bundle loaded
-            let bundles: string[] = [chunks["vendors~main"]];
+            let bundles: string[] = [chunks["vendors~main"].find((str: string) => str.endsWith(".js"))];
 
             const asyncChunk = getBundles(assetStats, modules);
 
@@ -57,9 +59,17 @@ function assetBuilder(assetStats: any, modules: any[]) {
             bundles = bundles.concat(asyncChunk.map((bundle: any) => bundle.file));
 
             // add main chunk as last.
-            bundles = bundles.concat([chunks.main]);
+            bundles = bundles.concat([chunks.main.find((str: string) => str.endsWith(".js"))]);
 
             return bundles.map((name) => buildTag(name)).join("\n");
+        },
+        getStyles: (): string => {
+            if (process.env.NODE_ENV === "development") {
+                return "";
+            }
+            const styles: string[] = [chunks.main.find((str: string) => str.endsWith(".css"))];
+
+            return styles.map((name) => buildStyle(name)).join("\n");
         },
     };
 }
